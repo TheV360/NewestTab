46cafe0f73975e10b3e80753e3d6481c5ca94492
V360 <v360code@gmail.com>
2020-03-19 02:14:37 -0400

    Looking okay

--- design.md
+++ design.md
@@ -1,26 +1,38 @@
-# NewestTab design doc.
+# NewestTab design
 
 ## Goals
 * Quick minimal lightweight tab page.
-* Inspired by some tab pages I've seen on r/UnixPorn
+* Inspired by some tab pages I've seen on r/UnixPorn and r/StartPages, the ones with a search bar and a few bookmarks in columns below it.
 
 ## Features
 * A sort of omnibar - of course like in chrome, but with user-selected keywords
-* Maybe a browser extension that replaces your new tab with it and integrates bookmarks?
-* Same background-selection features as in NewerTab, with blurring done on-the-fly with "Javascript making an image object" and not CSS filters.
+* Maybe a browser extension that replaces your new tab with it and integrates bookmarks? -- This would not search through all your bookmarks (that'd be horrible), you'd just select a few to copy over.
+* Same background-selection features as in NewerTab, with blurring done on-the-fly with "Javascript making an image object" and not CSS filters. This is for performance reasons. or maybe not do this?
 
-## Two view options
-Minimal
+## Three view modes
+### Minimal
+
+![thing](-designSketches/minimal.jpg)
+![thing](-designSketches/minimal2.jpg)
+
 * Just the search bar in the center. When you search, a list below pops out with some bookmarks.
 
-Bookmarks
-* Option to have a default list of bookmarks that shows up beneath the search bar.
+### Bookmarks
 
-Classic
-* Like NewerTab, but instead of the big icons, it'll be a static list of websites.
+![thing](-designSketches/bookmarks.png)
+
+* Option to have a default list of bookmarks that shows up beneath the search bar.
+* Ignore that "Search for..." it's broken and i fixed it already
+
+### Classic?????
+* All it is is Bookmarks with a big clock above the search bar
+* Meant to emulate NewerTab.
+* Maybe this is dumb and I shouldn't include it. Bookmarks mode is already pretty good and I don't want to deal with moving the clock out of the bar
 
 ## Specific things I have in mind
-* toggle for search bar to fill parent
-* search bar initially glass but will transition to solid white when you start typing.
-* "Search..." text on left, current date/time on right.
-* Maybe some basic favicon-kinda-thing with FontAwesome brands.svg and a look-up table?
+* ~~toggle for search bar to fill parent. otherwise it will have some padding that clearly says "I am not the entire parent"~~ see `bookmarks.png`
+* ~~search bar initially glass but will transition to solid white when you start typing.~~
+* ~~"Search..." text on left, current date/time on right.~~
+* Maybe some basic favicon-kinda-thing with FontAwesome brands.svg and a short look-up table? With the kinda "white icons on colorful squares" thing? This also means the look-up table will have color data
+* Color picker, when right-clicked, will change to a text field to allow for copying colors much more easily.
+* Oof, columns will be fun to design
---
+++ js/main.js
@@ -0,0 +1,131 @@
+var mainNode;
+var searchBar, results;
+
+var searchProvider = "https://google.com/search?q=%s";
+
+var database = [
+	{ name: "Amazon",		link: "https://amazon.com/",	},
+	{ name: "Bandcamp",		link: "https://bandcamp.com/",	},
+	{ name: "Bill Wurtz",	link: "https://billwurtz.com/",	},
+	{ name: "Reddit",		link: "https://reddit.com/hot/",},
+	{ name: "Twitter",		link: "https://twitter.com/",	},
+	{ name: "Tumblr",		link: "https://tumblr.com/",	},
+];
+
+// boxBlurImage(imageID,canvasID,radius,iterations)
+
+document.addEventListener("DOMContentLoaded", e=>{
+	// Remove all empty whitespace nodes.
+	clean(document.body);
+	
+	mainNode = document.getElementById("main");
+	
+	bar = document.getElementById("bar");
+	clock = document.getElementById("clock");
+	searchBar = document.getElementById("search");
+	
+	results = document.getElementById("results");
+	
+	searchBar.addEventListener("input", e=>updateSearch(e.target.value));
+	
+	updateClock();
+});
+
+function updateClock() {
+	var now = new Date();
+	const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	
+	// todo: a lot of things here.
+	// localetimestring is *very* useful, though. it'll also support multiple languages by default!!
+	
+	clock.textContent = now.toLocaleTimeString(undefined) + " " + now.toLocaleDateString(undefined, options);
+	
+	setTimeout(()=>{updateClock();}, .2e3);
+}
+
+function updateSearch(value) {
+	// A bit gross
+	if (value.length) {
+		mainNode.classList.add("on");
+		bar.classList.add("on");
+		searchBar.classList.add("on");
+	} else {
+		mainNode.classList.remove("on");
+		bar.classList.remove("on");
+		searchBar.classList.remove("on");
+	}
+	
+	var databaseResults = searchDatabase(value);
+	
+	results.innerHTML = "";
+	if (databaseResults.length) {
+		for (var i = 0; i < databaseResults.length; i++) {
+			results.appendChild(makeDatabaseResultNode(databaseResults[i]));
+		}
+	}
+	if (value.length) {
+		results.appendChild(makeSearchRequestNode(value));
+	}
+}
+
+function searchDatabase(term) {
+	var result = [];
+	
+	term = term.toLocaleLowerCase();
+	
+	for (var i = 0; i < database.length; i++) {
+		var tmp = database[i].name.toLocaleLowerCase().indexOf(term);
+		if (tmp >= 0) {
+			// just store some temporary data here
+			result.push([i, tmp]);
+		}
+	}
+	
+	// sort by how close to the beginning the term is.
+	result.sort((a, b)=>{return a[1] - b[1];});
+	
+	// remove temporary data, so it's just the indexes now.
+	for (var i = 0; i < result.length; i++) {
+		result[i] = result[i][0];
+	}
+	
+	return result;
+}
+
+function makeDatabaseResultNode(index) {
+	var node = document.createElement("li");
+	var nodeLink = document.createElement("a");
+	
+	nodeLink.textContent = database[index].name;
+	nodeLink.href = database[index].link;
+	
+	node.appendChild(nodeLink);
+	return node;
+}
+function makeSearchRequestNode(term) {
+	var node = document.createElement("li");
+	var nodeLink = document.createElement("a");
+	
+	// Fun way to get around having to filter things.
+	nodeLink.textContent = term;
+	nodeLink.innerHTML = "Search for <strong>" + nodeLink.innerHTML + "</strong>...";
+	
+	nodeLink.href = searchProvider.replace("%s", encodeURIComponent(term));
+	
+	node.appendChild(nodeLink);
+	return node;
+}
+
+// From sitepoint.com/removing-useless-nodes-from-the-dom/
+function clean(node) {
+	for (var n = 0; n < node.childNodes.length; n++) {
+		var child = node.childNodes[n];
+		
+		if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+			node.removeChild(child);
+			n--;
+		} else if (child.nodeType === 1 && !/pre|code|blockquote/i.test(child.tagName)) {
+			clean(child);
+		}
+	}
+}
